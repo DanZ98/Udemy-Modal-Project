96938c24166bad3cc50dce02525927201d26275a
Dan Zins <danzins15@gmail.com>
2021-10-14 22:18:14 -0500

    Completed modal project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,14 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+// retrieving classes
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+// Add click event to add open-modal class
+modalBtn.addEventListener('click', function(){
+    modalOverlay.classList.add('open-modal');
+});
+
+// Add click event to remove open-modal class
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal');
+});
